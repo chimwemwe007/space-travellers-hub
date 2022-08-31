4c894399c3f4cd33b60d954fa1d0ab962593d188
Chimwemwe Mkandawire <chimwe.wire@gmail.com>
2022-08-31 13:05:52 +0200

    Update rockets file

--- src/components/RocketsComponent.js
+++ src/components/RocketsComponent.js
@@ -1,10 +1,37 @@
-function RocketsComponent() {
+import React, { useEffect } from 'react';
+import { useSelector, useDispatch } from 'react-redux';
+import { fetchRockets } from '../../redux/rockets/rockets';
+
+
+// function RocketsComponent() {
+//   return (
+//     <div className="container">
+//       <br />
+//       <h3> Rockets Component</h3>
+//     </div>
+//   );
+// }
+
+const RocketsComponent = () => {
+  const rockets = useSelector((state) => state.rockets.rockets);
+  const dispatch = useDispatch();
+  useEffect(() => {
+    if (rockets.length === 0) {
+      dispatch(fetchRockets());
+    }
+  }, [dispatch]);
   return (
-    <div className="container">
-      <br />
-      <h3> Rockets Component</h3>
-    </div>
+    <>
+      {rockets.map((rocket) => (
+        <div key={rocket.id}>
+          <img src={rocket.images} alt={rocket.name} />
+          <h3>{rocket.name}</h3>
+          <p>{rocket.description}</p>
+        </div>
+      ))}
+    </>
   );
-}
+};
+
 
 export default RocketsComponent;
