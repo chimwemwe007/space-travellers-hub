fd190c06876e699ad89b19589571cb85a032c4bc
Chimwemwe Mkandawire <chimzymk@gmail.com>
2022-09-03 00:12:48 +0200

    Update Navbar.js
    add navlink as per review changes

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,26 +1,28 @@
 import React from 'react';
-import { Link } from 'react-router-dom';
+import { NavLink } from 'react-router-dom';
+//import { Link } from 'react-router-dom';
 import logo from './logo.png';
+
 
 function Navbar() {
   return (
     <nav className="navbar navbar-expand-sm panel-bg menu ">
       <div className="container">
-        <Link className="Bookstore-CMS" to="/">
+        <NavLink className="Bookstore-CMS" to="/" >
           <img className="logo" src={logo} alt="fireSpot" />
           Space Travelers Hub
-
+ 
         </Link>
 
         <ul className="navbar-nav ms-auto">
           <li className="nav-item">
-            <Link className="nav-link" to="/">Rockets</Link>
+            <NavLink className="nav-link" to="/">Rockets</NavLink>
           </li>
           <li className="nav-item">
-            <Link className="nav-link" to="/missions">Missions</Link>
+            <NavLink className="nav-link" to="/missions">Missions</NavLink>
           </li>
           <li className="nav-item">
-            <Link className="nav-link" to="/profile">My Profile</Link>
+            <NavLink className="nav-link" to="/profile">My Profile</NavLink>
           </li>
         </ul>
       </div>
